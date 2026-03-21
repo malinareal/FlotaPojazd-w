cedc2967ae3a8cf027caecbb0fba08085d67d455
Igor Rudziński <igor.rudzinski2005@gmail.com>
2026-03-21 07:18:27 +0100

    Dodano funkcje sprawdzpojazd() i aktualizuj przebieg()

--- main.cpp
+++ main.cpp
@@ -21,7 +21,57 @@ void zwrocPojazd() {
 }
 
 void sprawdzPojazdy() {
-    cout << "Funkcja sprawdzPojazdy() jeszcze nie jest zaimplementowana.\n";
+    ifstream file("json/pojazdy.json");
+
+    if (!file.is_open()) {
+        cout << "Nie mozna otworzyc pliku!\n";
+        return;
+    }
+
+    string linia, calosc;
+
+    while (getline(file, linia)) {
+        calosc += linia;
+    }
+
+    file.close();
+
+    size_t pos = 0;
+
+    while ((pos = calosc.find("{", pos)) != string::npos) {
+        size_t end = calosc.find("}", pos);
+        string obiekt = calosc.substr(pos, end - pos);
+
+        auto znajdz = [&](string klucz) {
+            size_t start = obiekt.find("\"" + klucz + "\"");
+            if (start == string::npos) return string("");
+
+            start = obiekt.find(":", start) + 1;
+
+            while (obiekt[start] == ' ' || obiekt[start] == '\"')
+                start++;
+
+            size_t stop = start;
+
+            if (obiekt[start - 1] == '\"') {
+                stop = obiekt.find("\"", start);
+            } else {
+                while (stop < obiekt.size() && isdigit(obiekt[stop]))
+                    stop++;
+            }
+
+            return obiekt.substr(start, stop - start);
+        };
+
+        string id = znajdz("id");
+        string marka = znajdz("marka");
+        string model = znajdz("model");
+
+        cout << "ID: " << id
+             << " | Nazwa: " << marka << " " << model << endl;
+
+        pos = end + 1;
+    }
 }
 
 void dodajPojazd()
@@ -117,7 +167,120 @@ void sprawdzStatusTechniczny() {
 }
 
 void aktualizujPrzebieg() {
-    cout << "Funkcja aktualizujPrzebieg() jeszcze nie jest zaimplementowana.\n";
+    ifstream file("json/pojazdy.json");
+
+    if (!file.is_open()) {
+        cout << "Nie mozna otworzyc pliku!\n";
+        return;
+    }
+
+    string linia, calosc;
+
+    while (getline(file, linia)) {
+        calosc += linia;
+    }
+    file.close();
+
+    size_t pos = 0;
+
+    cout << "\n===== LISTA POJAZDOW =====\n";
+
+    // 🔹 LISTOWANIE POJAZDÓW
+    while ((pos = calosc.find("{", pos)) != string::npos) {
+        size_t end = calosc.find("}", pos);
+        string obiekt = calosc.substr(pos, end - pos);
+
+        auto znajdz = [&](string klucz) {
+            size_t start = obiekt.find("\"" + klucz + "\"");
+            if (start == string::npos) return string("");
+
+            start = obiekt.find(":", start) + 1;
+
+            while (obiekt[start] == ' ' || obiekt[start] == '\"')
+                start++;
+
+            size_t stop = start;
+
+            if (obiekt[start - 1] == '\"') {
+                stop = obiekt.find("\"", start);
+            } else {
+                while (stop < obiekt.size() && isdigit(obiekt[stop]))
+                    stop++;
+            }
+
+            return obiekt.substr(start, stop - start);
+        };
+
+        string id = znajdz("id");
+        string marka = znajdz("marka");
+        string model = znajdz("model");
+        string przebieg = znajdz("przebieg");
+
+        cout << "ID: " << id
+             << " | " << marka << " " << model << przebieg << endl;
+
+        pos = end + 1;
+    }
+
+    // 🔹 WYBÓR POJAZDU
+    string szukaneID;
+    cout << "\nPodaj ID pojazdu: ";
+    cin >> szukaneID;
+
+    string nowyPrzebieg;
+    cout << "Podaj nowy przebieg: ";
+    cin >> nowyPrzebieg;
+
+    pos = 0;
+    bool znaleziono = false;
+
+    // 🔹 AKTUALIZACJA
+    while ((pos = calosc.find("{", pos)) != string::npos) {
+        size_t end = calosc.find("}", pos);
+        string obiekt = calosc.substr(pos, end - pos);
+
+        size_t idPos = obiekt.find("\"id\"");
+        if (idPos != string::npos) {
+            size_t start = obiekt.find(":", idPos) + 1;
+            while (obiekt[start] == ' ') start++;
+
+            size_t stop = start;
+            while (isdigit(obiekt[stop])) stop++;
+
+            string id = obiekt.substr(start, stop - start);
+
+            if (id == szukaneID) {
+                size_t przebiegPos = calosc.find("\"przebieg\"", pos);
+
+                if (przebiegPos != string::npos) {
+                    size_t valStart = calosc.find(":", przebiegPos) + 1;
+
+                    while (calosc[valStart] == ' ') valStart++;
+
+                    size_t valEnd = valStart;
+                    while (isdigit(calosc[valEnd])) valEnd++;
+
+                    calosc.replace(valStart, valEnd - valStart, nowyPrzebieg);
+
+                    znaleziono = true;
+                }
+                break;
+            }
+        }
+
+        pos = end + 1;
+    }
+
+    if (!znaleziono) {
+        cout << "Nie znaleziono pojazdu!\n";
+        return;
+    }
+
+    ofstream out("json/pojazdy.json");
+    out << calosc;
+    out.close();
+
+    cout << "Przebieg zaktualizowany!\n";
 }
 
 // ======== MENU ========
